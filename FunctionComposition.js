32006a64108b9689180ef0f9d6fe1ec6bda8702d
Ahmed Hossam <ahmedhossam9226@gmail.com>
2023-12-22 11:59:54 +0200

    leetCode function composition problem : Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.

---
+++ FunctionComposition.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+  return function(x) {
+      return functions.reduceRight((acc, fn) => fn(acc), x);
+  }
+};
+
+// Test cases
+let functions = [x => x + 1, x => x * x, x => 2 * x];
+let fn = compose(functions);
+console.log(fn(4)); // Output: 65
+
+functions = [x => 10 * x, x => 10 * x, x => 10 * x];
+fn = compose(functions);
+console.log(fn(1)); // Output: 1000
+
+functions = [];
+fn = compose(functions);
+console.log(fn(42)); // Output: 42
